0b137bcd2bc40be2a1621cdd05542e64f4e9c9ae
Pratick Bhowmick <43977656+pratsb13@users.noreply.github.com>
2024-07-26 15:39:43 +0530

    Add files via upload

---
+++ undirected_graph_ds/src/Graph.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graph {
+    
+    List<List<Integer>> graph; // The indices of the graph list will serve as the vertices
+
+    public Graph(int numVertices) {
+        this.graph = new ArrayList<>(numVertices); // Create numVertices vertices in the graph
+        for (int i = 0; i < numVertices; i++) {
+            this.graph.add(new ArrayList<>(numVertices-1)); // For each vertex, add placeholder for edge to other vertices 
+        }
+    }
+
+    public void addEdge(int u, int v) { // Because this is an undirected graph edge vertices u and v will be connected to each other
+        this.graph.get(u).add(v);
+        this.graph.get(v).add(u);
+    }
+    
+    public void print() {
+        for (int i = 0; i < graph.size(); i++) {
+            System.out.println("Vertex " + i);
+            List<Integer> currentVertex = this.graph.get(i);
+
+            for (int j = 0; j < currentVertex.size(); j++) {
+                System.out.println(i + " -> " + currentVertex.get(j));
+            }
+        }
+    }
+
+}
